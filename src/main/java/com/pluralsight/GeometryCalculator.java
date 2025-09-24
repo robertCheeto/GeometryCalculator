4c4148c2138a8abe1e92015d68a512143d5335ce
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-09-24 15:21:26 -0400

    * added code to find the circumference of a circle * added code to calculate rectangle area and perimeter

--- src/main/java/com/pluralsight/GeometryCalculator.java
+++ src/main/java/com/pluralsight/GeometryCalculator.java
@@ -2,6 +2,7 @@ package com.pluralsight;
 
 public class GeometryCalculator {
     public static void main(String[] args) {
+        // Part 1 - Circle Area
         double circleRadius = 5.5;
         double rectangleLength = 8.2;
         double rectangleWidth = 3.7;
@@ -11,6 +12,19 @@ public class GeometryCalculator {
         System.out.println("Calculating the are of a circle of the radius is " + circleRadius + "...");
         double circleArea = Math.PI * Math.pow(circleRadius, 2);
         System.out.printf("The area of that circle would be: %.2f\n", circleArea);
+
+        // Part 2 - Circle Circumference
+        System.out.println("Calculating the circumference of the Circle...");
+        double circleCircumference = (2 * Math.PI * circleRadius);
+        System.out.printf("The circumference of the circle would be: %.2f\n\n\n", circleCircumference);
+
+        // Part 3 - Rectangle Area and Perimeter
+        System.out.println("Calculating the Area of a Rectangle with a length of " + rectangleLength + " and a width of " + rectangleWidth + ".");
+        double rectangleArea = rectangleWidth * rectangleLength;
+        System.out.printf("The area of that rectangle is: %.2f\n", rectangleArea);
+        System.out.println("Calculating the Perimeter of that same Rectangle...");
+        double rectanglePerimeter = 2 * (rectangleWidth + rectangleLength);
+        System.out.printf("The perimeter of that rectangle is: %.2f\n\n\n", rectanglePerimeter);
 
 
     }
