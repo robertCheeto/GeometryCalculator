c17d916a4abae271951c9e7c2b71d7a6537614a2
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-09-24 15:38:22 -0400

    * added context for squared units * started working on step 4 - triangle area

--- src/main/java/com/pluralsight/GeometryCalculator.java
+++ src/main/java/com/pluralsight/GeometryCalculator.java
@@ -9,23 +9,30 @@ public class GeometryCalculator {
         double triangleBase = 6.0;
         double triangleHeight = 4.5;
 
-        System.out.println("Calculating the are of a circle of the radius is " + circleRadius + "...");
+        System.out.println("Calculating the are of a circle if the radius is " + circleRadius + " units...");
         double circleArea = Math.PI * Math.pow(circleRadius, 2);
-        System.out.printf("The area of that circle would be: %.2f\n", circleArea);
+        System.out.printf("The area of that circle would be: %.2f square units.\n", circleArea);
 
         // Part 2 - Circle Circumference
         System.out.println("Calculating the circumference of the Circle...");
         double circleCircumference = (2 * Math.PI * circleRadius);
-        System.out.printf("The circumference of the circle would be: %.2f\n\n\n", circleCircumference);
+        System.out.printf("The circumference of the circle would be: %.2f units.\n\n\n", circleCircumference);
 
         // Part 3 - Rectangle Area and Perimeter
-        System.out.println("Calculating the Area of a Rectangle with a length of " + rectangleLength + " and a width of " + rectangleWidth + ".");
+        System.out.println("Calculating the Area of a Rectangle with a length of " + rectangleLength + " units and a width of " + rectangleWidth + " units.");
         double rectangleArea = rectangleWidth * rectangleLength;
-        System.out.printf("The area of that rectangle is: %.2f\n", rectangleArea);
+        System.out.printf("The area of that rectangle is: %.2f square units.\n", rectangleArea);
         System.out.println("Calculating the Perimeter of that same Rectangle...");
         double rectanglePerimeter = 2 * (rectangleWidth + rectangleLength);
-        System.out.printf("The perimeter of that rectangle is: %.2f\n\n\n", rectanglePerimeter);
+        System.out.printf("The perimeter of that rectangle is: %.2f units.\n\n\n", rectanglePerimeter);
 
+        // Step 4 - Calculate Triangle Area with Math.sqrt() to find the Hypotenuse
+        // area = (1/2) * base * height
+        System.out.println("Calculating the Area of a Triangle with a base of " + triangleBase + " units and a height of " + triangleHeight + " units.");
+        double triangleArea = (triangleBase * triangleHeight * (1/2));
+        System.out.printf("The area of that Triangle would be %.2f units.\n", triangleArea);
+        double triangleHypotenuse = Math.sqrt(Math.pow(triangleBase, 2) + Math.pow(triangleHeight, 2));
+        System.out.printf("The hypotenuse of the Triangle would be %.2f units.", triangleHypotenuse);
 
     }
 }
